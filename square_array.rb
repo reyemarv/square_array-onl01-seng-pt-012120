4c196733f86ce0e6b7c145ec3e6daae8f549e40f
Marvin Reyes <reyemarv@gmail.com>
2020-02-09 19:27:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  new_array = []
-  array.each do |num|
-    puts new_array.push(num ** 2)  
-  end 
-  new_array
+ new_array = []
+ array.each do |num|
+   new_array.push(num ** 2)
+ end 
+ new_array
 end
